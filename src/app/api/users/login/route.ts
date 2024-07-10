f3d3caf1cae36aa27d6fca93a20783da1e68a497
Vaibhav Gupta <109146556+vaibhavgupta5@users.noreply.github.com>
2024-07-10 13:10:14 +0530

    Update route.ts

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -4,20 +4,23 @@ import {NextRequest, NextResponse} from 'next/server'
 import bcryptjs from 'bcryptjs'
 import jwt from "jsonwebtoken"
 
-connect();
-
+connect(); // need to call connect func each time
+// request: NextRequest takes data from frontend
 export async function POST(request: NextRequest){
     try {
-        const reqBody = await request.json()
+        //covertes into json format
+        const reqBody = await request.json() 
         const {email, password} = reqBody
         console.log(reqBody)
 
+        //finds a user from database
         const user = await User.findOne({email})
 
         if(!user){
             return NextResponse.json({error: "User Not Exists, SignUp"},{status: 400})
         }
 
+        // compared entered password to database pass using compare
         const validPassword = await bcryptjs.compare(password, user.password)
 
         if(!validPassword){
@@ -30,14 +33,18 @@ export async function POST(request: NextRequest){
             email: user.email
         }
 
+        //creating token using jwt and feeds above tokenData
         const token = await jwt.sign({tokenData}, process.env.SECRET_TOKEN!, {expiresIn: "1d"})
 
+        // sends response in frontend 
         const response = NextResponse.json({
             message: "Logged in Success"}, {status: 200}
         )
 
+        //no need of cookie-parser, have directly access of cookies.
         response.cookies.set("token", token, {
             httpOnly: true,
+            //httpOnly makes cookies only readable
         })
 
         return response
