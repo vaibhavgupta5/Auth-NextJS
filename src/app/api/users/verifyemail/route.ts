0fca06014ea5a9385cc03074480f98ef18deff0f
Vaibhav Gupta <109146556+vaibhavgupta5@users.noreply.github.com>
2024-07-10 13:15:40 +0530

    Update route.ts

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -11,15 +11,12 @@ export async function POST(request: NextRequest){
         
         const reqBody = await request.json()
         const {token} = reqBody
-        console.log(token)
 
         const user = await User.findOne({verifyToken : token, verifyTokenExpiry: {$gt : Date.now()}})
 
         if(!user){
             return NextResponse.json({error: "No user Found"}, {status: 500})
         }
-
-        console.log(user)
 
         user.isVerified = true;
         user.verifyToken = undefined;
