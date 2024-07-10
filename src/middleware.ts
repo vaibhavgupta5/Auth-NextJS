e969b5411282fbd0a1968e768eabb84296628176
Vaibhav Gupta <109146556+vaibhavgupta5@users.noreply.github.com>
2024-07-10 22:24:00 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,7 +25,7 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     "/",
-    "/login",
+    "/Login",
     "/SignUp",
     "/profile",
     "/verifyemail"
